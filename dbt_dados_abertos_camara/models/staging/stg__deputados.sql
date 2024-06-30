d13cb8d612d53c5c8610e25d29ecdb21e1cedf49
ClaudioPontes <claudiohpontes@gmail.com>
2024-06-30 11:32:06 -0300

    Adicionado .source.yml

--- dbt_dados_abertos_camara/models/staging/stg__deputados.sql
+++ dbt_dados_abertos_camara/models/staging/stg__deputados.sql
@@ -1,13 +1,13 @@
-WITH
-	renamed AS
-(
-SELECT
+with source as (
+      select * from {{ source('dados_abertos', 'lz_deputados') }}
+),
+renamed as (
+    select    
 	deputados.id AS id_deputado,
 	deputados.nome,
 	deputados."siglaPartido" AS sigla_partido,
 	deputados."siglaUf"  AS uf,
 	deputados."urlFoto" AS url_foto
-FROM
-	{{ref('raw__deputados')}}  deputados
+    from source deputados
 )
-SELECT * FROM renamed
+select * from renamed
