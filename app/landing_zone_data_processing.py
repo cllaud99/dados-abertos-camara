9e2c7bc6d61ff501961a1a7b0b4669443a7b448a
ClaudioPontes <claudiohpontes@gmail.com>
2024-07-08 21:33:24 -0300

    Refatoradas landing_zone  docstrings e tratamentos de erros

--- app/landing_zone_data_processing.py
+++ app/landing_zone_data_processing.py
@@ -1,16 +1,18 @@
 import json
 from pathlib import Path
-from typing import Type
+from typing import Type, List
 
+from loguru import logger
 import pandas as pd
 from models import Despesa
 from pydantic import BaseModel, ValidationError
 
 
-def read_and_validate_json(file_path: Path, model: Type[BaseModel]):
+def read_and_validate_json(file_path: Path, model: Type[BaseModel]) -> List[BaseModel]:
     """
     Lê um arquivo JSON do caminho especificado, valida seus dados usando um modelo Pydantic fornecido
     e retorna uma lista de instâncias do modelo.
+
     Args:
         file_path (Path): Caminho para o arquivo JSON a ser lido e validado.
         model (Type[BaseModel]): Classe Pydantic que define a estrutura esperada dos dados JSON.
@@ -23,59 +25,55 @@ def read_and_validate_json(file_path: Path, model: Type[BaseModel]):
         with open(str(file_path), "r", encoding="utf-8") as file:
             json_data = json.load(file)
 
-            # Caso 1: JSON é uma lista de objetos
             if isinstance(json_data, list):
-                try:
-                    items = [model(**item) for item in json_data]
-                    return items
-                except ValidationError as e:
-                    print(f"Erro de validação no arquivo {file_path}: {e}")
-                    return None
-
-            # Caso 2: Objeto com chave 'dados' contendo lista
+                items = [model(**item) for item in json_data]
             elif isinstance(json_data.get("dados"), list):
-                try:
-                    items = [model(**item) for item in json_data["dados"]]
-                    return items
-                except ValidationError as e:
-                    print(f"Erro de validação no arquivo {file_path}: {e}")
-                    return None
-
-            # Caso 3: Objeto com chave 'dados' que não é lista
+                items = [model(**item) for item in json_data["dados"]]
             elif isinstance(json_data.get("dados"), dict):
-                try:
-                    items = [model(**json_data["dados"])]
-                    return items
-                except ValidationError as e:
-                    print(f"Erro de validação no arquivo {file_path}: {e}")
-                    return None
-        print(f"Estrutura inválida no arquivo {file_path}.")
-        return None
+                items = [model(**json_data["dados"])]
+            else:
+                logger.error(f"Estrutura inválida no arquivo {file_path}.")
+                return None
+
+            logger.info(f"Dados lidos e validados com sucesso do arquivo {file_path}.")
+            return items
 
     except FileNotFoundError:
-        print(f"Arquivo '{file_path}' não encontrado.")
-        return None
+        logger.error(f"Arquivo '{file_path}' não encontrado.")
     except json.JSONDecodeError as e:
-        print(f"Erro ao decodificar JSON: {e}")
-        return None
+        logger.error(f"Erro ao decodificar JSON no arquivo {file_path}: {e}")
     except ValidationError as e:
-        print("Erro de validação:")
-        print(e.json())
-        return None
+        logger.error(f"Erro de validação no arquivo {file_path}: {e}")
+        logger.error(f"Dados inválidos: {e.json()}")
+
+    return None
 
 
 def normalize_columns_with_json(df):
-    for column in df.columns:
-        try:
-            # Tentar carregar o conteúdo da coluna como JSON
-            df[column] = df[column].apply(json.loads)
-            
-            # Normalizar apenas se o conteúdo for um dicionário
-            if df[column].apply(type).eq(dict).all():
-                df_normalized = pd.json_normalize(df[column])
-                df = pd.concat([df.drop(columns=[column]), df_normalized], axis=1)
-        except (json.JSONDecodeError, ValueError):
-            # Ignorar a coluna se não puder ser carregada como JSON
-            pass
+    """
+    Tenta carregar o conteúdo de cada coluna do DataFrame como JSON e normaliza as colunas
+    que contêm dicionários JSON em novas colunas no DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame contendo as colunas a serem normalizadas.
+
+    Returns:
+        pd.DataFrame: DataFrame atualizado com colunas normalizadas, se aplicável.
+    """
+    try:
+        for column in df.columns:
+            try:
+                df[column] = df[column].apply(json.loads)
+                if df[column].apply(type).eq(dict).all():
+                    df_normalized = pd.json_normalize(df[column])
+                    df = pd.concat([df.drop(columns=[column]), df_normalized], axis=1)
+            except (json.JSONDecodeError, ValueError) as e:
+                logger.warning(f"Erro ao carregar conteúdo da coluna {column} como JSON: {e}")
+                pass
+        
+        logger.info("Colunas normalizadas com sucesso.")
+        return df
     
-    return df
+    except Exception as e:
+        logger.error(f"Erro ao normalizar colunas com JSON: {e}")
+        raise
