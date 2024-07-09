b6c06f306a95d8cd0fac8672b4bdb751a2e240b3
ClaudioPontes <claudiohpontes@gmail.com>
2024-07-08 23:01:58 -0300

    fix historico deputados

--- app/data_ingestion.py
+++ app/data_ingestion.py
@@ -13,7 +13,7 @@ from db_operations import (
     validate_postgresql_connection,
 )
 from get_api_data import fetch_all_data, fetch_data, get_ids_deputados, save_to_raw
-from landing_zone_data_processing import read_and_validate_json, normalize_columns_with_json
+from landing_zone_data_processing import read_and_validate_json
 from models import DadosDeputado, Deputado, Despesa, DeputadoHistorico
 from sqlalchemy import create_engine
 from tqdm import tqdm
@@ -21,7 +21,7 @@ from tqdm import tqdm
 
 load_dotenv()
 
-sample_run = os.getenv('SAMPLE_RUN')
+sample_run = os.getenv("SAMPLE_RUN")
 print(sample_run)
 ID_LEGISLATURA = 57
 
@@ -38,7 +38,9 @@ def download_data(sample_run="False"):
     """
     try:
         path_deputados_file = "data/landing_zone/deputados_raw.json"
-        deputados = fetch_data("https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura=57")
+        deputados = fetch_data(
+            "https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura=57"
+        )
         save_to_raw(deputados, path_deputados_file)
 
         ids = get_ids_deputados(path_deputados_file)
@@ -53,13 +55,17 @@ def download_data(sample_run="False"):
             file_path_infos = f"data/landing_zone/infos/{id}_infos.json"
             save_to_raw(data_infos, file_path_infos)
 
-            url_despesas = f"https://dadosabertos.camara.leg.br/api/v2/deputados/{id}/despesas"
+            url_despesas = (
+                f"https://dadosabertos.camara.leg.br/api/v2/deputados/{id}/despesas"
+            )
             params_despesas = {"idLegislatura": ID_LEGISLATURA}
             data_despesas = fetch_all_data(url_despesas, params=params_despesas)
             file_path_despesas = f"data/landing_zone/despesas/{id}_despesas.json"
             save_to_raw(data_despesas, file_path_despesas)
 
-            url_historico = f"https://dadosabertos.camara.leg.br/api/v2/deputados/{id}/historico"
+            url_historico = (
+                f"https://dadosabertos.camara.leg.br/api/v2/deputados/{id}/historico"
+            )
             data_historico = fetch_all_data(url_historico)
             file_path_historico = f"data/landing_zone/historico/{id}_historico.json"
             save_to_raw(data_historico, file_path_historico)
@@ -83,7 +89,9 @@ def normalize_and_save(json_folder, model, table_name, engine):
     """
     try:
         if validate_postgresql_connection(engine):
-            for file_path in tqdm(list(json_folder.glob("*.json")), desc="Processing files"):
+            for file_path in tqdm(
+                list(json_folder.glob("*.json")), desc="Processing files"
+            ):
                 try:
                     validated_items = read_and_validate_json(file_path, model)
                     if validated_items:
@@ -93,16 +101,23 @@ def normalize_and_save(json_folder, model, table_name, engine):
                         for item in validated_items:
                             # Serializar UltimoStatus para JSON
                             item_dict = item.model_dump()
-                            if 'ultimoStatus' in item_dict and item_dict['ultimoStatus'] is not None:
-                                item_dict['ultimoStatus'] = json.dumps(item_dict['ultimoStatus'])
+                            if (
+                                "ultimoStatus" in item_dict
+                                and item_dict["ultimoStatus"] is not None
+                            ):
+                                item_dict["ultimoStatus"] = json.dumps(
+                                    item_dict["ultimoStatus"]
+                                )
 
-                            item_dict['file_name'] = Path(file_path).name
+                            item_dict["file_name"] = Path(file_path).name
                             data_to_insert.append(item_dict)
 
                         df = pd.DataFrame(data_to_insert)
 
                         insert_data_to_postgres(df, table_name, engine)
-                        logger.info(f"Dados do arquivo {file_path} inseridos com sucesso na tabela {table_name}.")
+                        logger.info(
+                            f"Dados do arquivo {file_path} inseridos com sucesso na tabela {table_name}."
+                        )
 
                     else:
                         logger.warning(f"Nenhum dado válido no arquivo {file_path}.")
@@ -111,7 +126,9 @@ def normalize_and_save(json_folder, model, table_name, engine):
                     logger.error(f"Erro ao processar o arquivo {file_path}: {e}")
 
         else:
-            logger.error("Falha na conexão com o PostgreSQL. Verifique as configurações de conexão.")
+            logger.error(
+                "Falha na conexão com o PostgreSQL. Verifique as configurações de conexão."
+            )
 
     except Exception as e:
         logger.error(f"Ocorreu um erro geral: {e}")
@@ -120,9 +137,18 @@ def normalize_and_save(json_folder, model, table_name, engine):
 
 if __name__ == "__main__":
     download_data()
-    create_postgres_schema(engine, 'public')
+    create_postgres_schema(engine, "public")
     drop_all_tables_in_schema(engine, "public")
     normalize_and_save(Path("data/landing_zone/"), Deputado, "lz_deputados", engine)
-    normalize_and_save(Path("data/landing_zone/despesas"), Despesa, "lz_despesas", engine)
-    normalize_and_save(Path("data/landing_zone/infos"), DadosDeputado, "lz_infos_extras", engine)
-    normalize_and_save(Path("data/landing_zone/historico"), DeputadoHistorico, "lz_historico_deputado", engine)
+    normalize_and_save(
+        Path("data/landing_zone/despesas"), Despesa, "lz_despesas", engine
+    )
+    normalize_and_save(
+        Path("data/landing_zone/infos"), DadosDeputado, "lz_infos_extras", engine
+    )
+    normalize_and_save(
+        Path("data/landing_zone/historico"),
+        DeputadoHistorico,
+        "lz_historico_deputado",
+        engine,
+    )
--- dbt_dados_abertos_camara/models/staging/stg__historico_deputado.sql
+++ dbt_dados_abertos_camara/models/staging/stg__historico_deputado.sql
@@ -7,7 +7,8 @@ SELECT
 	id AS id_deputado,
 	"siglaPartido" AS sigla_partido,
 	"descricaoStatus" AS descricao_status,
-	CAST( "dataHora" AS date ) AS dt_evento 
+	CAST( "dataHora" AS date ) AS dt_evento,
+	"idLegislatura" as id_legislatura
 FROM
 	source rd
 )
