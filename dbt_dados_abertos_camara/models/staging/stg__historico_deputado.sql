a211b745d0a4a68759fb2fefcf8440446088a456
ClaudioPontes <claudiohpontes@gmail.com>
2024-07-08 21:48:07 -0300

    adicionado historico_deputado

---
+++ dbt_dados_abertos_camara/models/staging/stg__historico_deputado.sql
@@ -0,0 +1,14 @@
+with source as (
+      select * from {{ source('dados_abertos', 'lz_historico_deputado') }}
+),
+renamed AS
+(
+SELECT
+	id AS id_deputado,
+	"siglaPartido" AS sigla_partido,
+	"descricaoStatus" AS descricao_status,
+	CAST( "dataHora" AS date ) AS dt_evento 
+FROM
+	source rd
+)
+SELECT * FROM renamed
